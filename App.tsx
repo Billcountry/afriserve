70cace81f0ac616f78fe698c3eadb1c1357e43c0
Billcountry <me@billcountry.tech>
2021-03-09 20:01:25 +0300

    Putting it together

--- App.tsx
+++ App.tsx
@@ -1,13 +1,35 @@
 import React from "react"
 import { SafeAreaView, StatusBar } from "react-native"
+import { GlobalState, GlobalContext } from "./src/data/state"
+import auth from "@react-native-firebase/auth"
+import { Login } from "./src/components/auth/login"
+import { Splash } from "./src/components/splash"
 
 export default class App extends React.Component {
+    globalState = new GlobalState()
+    subscriber: any
+    componentDidMount() {
+        this.subscriber = auth().onAuthStateChanged((user) => {
+            this.globalState.authorized = user
+            if (user === null) {
+                this.globalState.page = "login"
+                this.globalState.loading = false
+            }
+        })
+    }
+    componentWillUnmount() {
+        if (this.subscriber) this.subscriber()
+    }
     render() {
+        const state = this.globalState
         return (
-            <>
+            <GlobalContext.Provider value={this.globalState}>
                 <StatusBar barStyle="dark-content" />
-                <SafeAreaView></SafeAreaView>
-            </>
+                <SafeAreaView>
+                    {state.page === "splash" && <Splash />}
+                    {state.page === "login" && <Login />}
+                </SafeAreaView>
+            </GlobalContext.Provider>
         )
     }
 }
