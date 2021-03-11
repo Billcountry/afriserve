b20fc2ac6cdd05ce7b3e24e0a4e358d8a2d0ddab
Billcountry <me@billcountry.tech>
2021-03-11 15:19:24 +0300

    Keep active shop in state

--- src/data/state.ts
+++ src/data/state.ts
@@ -1,11 +1,14 @@
 import { createContext } from "react"
 import { observable } from "mobx"
 import { FirebaseAuthTypes } from "@react-native-firebase/auth"
+import { Shop } from "./shop"
 
 export class GlobalState {
     @observable authorized: FirebaseAuthTypes.User | null = null
     @observable loading: boolean = true
-    @observable page: "login" | "register" | "activate" | "splash" = "splash"
+    @observable page: "login" | "register" | "activate" | "splash" | "error" =
+        "splash"
+    @observable shop: Shop | null = null
 }
 
 export const GlobalContext = createContext(new GlobalState())
