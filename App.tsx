59f7eeb3047caf3cefdca32520237da56bafd352
Billcountry <me@billcountry.tech>
2021-03-11 15:21:35 +0300

    Registration configuration

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import React from "react"
 import { observer } from "mobx-react"
-import { Container, StyleProvider, Root } from "native-base"
+import { Container, StyleProvider, Root, Toast } from "native-base"
 import { GlobalState, GlobalContext } from "./src/data/state"
 import { Login } from "./src/components/auth/login"
+import { Register } from "./src/components/auth/register"
 import { Splash } from "./src/components/splash"
+import { Error } from "./src/components/error"
 import getTheme from "./native-base-theme/components/index"
 import common from "./native-base-theme/variables/commonColor"
 import auth from "@react-native-firebase/auth"
+import firestore from "@react-native-firebase/firestore"
 
 @observer
 export default class App extends React.Component {
@@ -18,15 +21,38 @@ export default class App extends React.Component {
             if (user === null) {
                 this.globalState.page = "login"
                 this.globalState.loading = false
+                this.globalState.shop = null
                 return
             }
             // Set the page to splash and check if the user exists in firebase.
             this.globalState.page = "splash"
             this.globalState.loading = true
+            this.guessPage()
         })
     }
     componentWillUnmount() {
         if (this.subscriber) this.subscriber()
+    }
+    guessPage() {
+        const user = this.globalState.authorized
+        if (user === null) return
+        // Check if the user belongs to a shop and if the shop is activated.
+        firestore()
+            .collection("ShopUsers")
+            .where("UserId", "==", user.uid)
+            .get()
+            .then((querySnapshot) => {
+                if (querySnapshot.size) {
+                    // User already belongs to a shop. Login to the top shop
+                } else {
+                    // Take user to the registration page
+                    this.globalState.page = "register"
+                }
+            })
+            .catch((err) => {
+                //Maybe show an error page
+                this.globalState.page = "error"
+            })
     }
     render() {
         const state = this.globalState
@@ -37,6 +63,8 @@ export default class App extends React.Component {
                         <Container>
                             {state.page === "splash" && <Splash />}
                             {state.page === "login" && <Login />}
+                            {state.page === "register" && <Register />}
+                            {state.page === "error" && <Error />}
                         </Container>
                     </GlobalContext.Provider>
                 </StyleProvider>
