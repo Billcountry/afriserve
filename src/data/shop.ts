8d18f9c4eb68e84bd0b0c41ad122ec66c95a74a7
Billcountry <me@billcountry.tech>
2021-03-14 11:29:53 +0300

    Check if a shop has a specific phone registered

--- src/data/shop.ts
+++ src/data/shop.ts
@@ -1,5 +1,8 @@
 import { LiveData } from "./livedata"
 import { observable } from "mobx"
+import firestore from "@react-native-firebase/firestore"
+import { Customer } from "./customers"
+import { getValidPhone } from "../utils"
 
 export class Shop extends LiveData {
     @observable Name: string = ""
@@ -9,6 +12,19 @@ export class Shop extends LiveData {
     @observable Verified: boolean = false
     fields() {
         return ["Name", "Location", "Address", "Registered", "Verified"]
+    }
+
+    async hasPhone(phone: string): Promise<Customer | null> {
+        const query = firestore()
+            .collection(`${Customer.name}s`)
+            .where(`ShopId`, "==", this.doc.id)
+            .where(`Phone`, "==", phone)
+        const result = await query.get()
+        if (!result.empty) {
+            const doc = result.docs[0]
+            return new Customer(doc.id, doc)
+        }
+        return null
     }
 }
 
